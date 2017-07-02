4d0f50f173767c1dba801f8e9ea806f359a87534
Ryan Jones <ryan@system88.com>
2017-07-01 19:59:59 -0600

    proper uuid support

--- db/migrate/20170630024400_devise_create_users.rb
+++ db/migrate/20170630024400_devise_create_users.rb
@@ -1,7 +1,5 @@
 class DeviseCreateUsers < ActiveRecord::Migration[5.1]
   def change
-    enable_extension 'uuid-ossp'
-    
     create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
--- db/migrate/20170702013423_create_agile_teams.rb
+++ db/migrate/20170702013423_create_agile_teams.rb
@@ -1,6 +1,7 @@
 class CreateAgileTeams < ActiveRecord::Migration[5.1]
   def change
-    
+    enable_extension 'pgcrypto'
+
     create_table :agile_teams, id: :uuid do |t|
       t.string :name
 
