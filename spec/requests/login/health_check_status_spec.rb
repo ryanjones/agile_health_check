ca37cc795838aa119eb22ef26171e39147bd264d
Ryan Jones <ryan@system88.com>
2017-08-07 13:17:25 -0600

    start to build out the scenario of a team that's improving. ill need to create a bunch of score card answers to prove out the scenario

--- spec/requests/login/health_check_status_spec.rb
+++ spec/requests/login/health_check_status_spec.rb
@@ -1,6 +1,7 @@
 feature 'health check status' do
-  let(:agile_team) { FactoryGirl.create(:agile_team) }
-  let(:score_card_answer) { FactoryGirl.create(:score_card_answer) }
+  let(:score_card) { FactoryGirl.create(:score_card) }
+  let(:team_score_card_answer) { FactoryGirl.create(:team_score_card_answer, score_card: score_card) }
+  let(:application_score_card_answer) { FactoryGirl.create(:application_score_card_answer, score_card: score_card) }
   
   let(:user) do
     user = User.create!(email: "user@example.org", password: "very-secret")
@@ -9,16 +10,15 @@ feature 'health check status' do
   end
 
   it 'should show that an agile team has improved', js: true do
-    score_card_answer
+    score_card
+    team_score_card_answer
+    application_score_card_answer
     user
-    visit(agile_team_path(score_card_answer.score_card.agile_team))
+    visit(agile_team_path(score_card.agile_team))
 
-    save_and_open_screenshot
-    click_button 'Create a new health check'
-
-    expect(page).to have_content('Health check created successfully.')
-    expect(page).to have_content('Delivering Value - Agile')
-    expect(page).to have_content('Automated Tests - Application')
-    expect(page).not_to have_content('Fun - Product Owner')
+    expect(page).to have_content('0%')
   end
+
+  pending 'should show that a team is not declining or improving'
+  pending 'should show that a team has declined'
 end
